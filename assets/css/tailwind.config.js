5a7bbd99c3898efa493f80f255aeda6f3197c16a
Vantablack <chongyaowei@gmail.com>
2021-01-09 16:47:57 +0800

    Added basic Tailwind color variables

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,62 +1,80 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   important: true,
   theme: {
     extend: {
+      colors: {
+        'primary': colors.gray[900],
+        'primary-hover': colors.blue[700],
+        'secondary': colors.gray[700],
+        'secondary-hover': colors.blue[700],
+        // 'accent': colors.blue[700],
+        'primary-dark': colors.gray[300],
+        'secondary-dark': colors.gray[400],
+        'secondary-hover-dark': colors.blue[500],
+        // 'accent-dark': colors.blue[500],
+      },
+      backgroundColor: {
+        'primary': colors.white,
+        'primary-dark': colors.coolGray[900],
+      },
       typography: (theme) => ({
-        // DEFAULT: {
-        //   css: {
-        //     color: theme("colors.gray.900"),
-        //     a: {
-        //       color: theme("colors.blue.700"),
-        //       "&:hover": {
-        //         color: theme("colors.blue.700"),
-        //       },
-        //     },
-        //   },
-        // },
+        DEFAULT: {
+          css: {
+            color: theme("colors.primary"),
+            // a: {
+            //   color: theme("colors.primary"),
+            //   "&:hover": {
+            //     color: theme("colors.primary-hover"),
+            //   },
+            // },
+          },
+        },
         dark: {
           css: {
-            color: theme("colors.gray.300"),
+            color: theme("colors.primary-dark"),
             a: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.secondary-dark"),
+              // "&:hover": {
+              //   color: theme("colors.secondary-hover-dark"),
+              // },
+            },
+            "a code": {
+              color: theme("colors.secondary-dark"),
               "&:hover": {
-                color: theme("colors.gray.300"),
+                color: theme("colors.secondary-hover-dark"),
               },
             },
-
             h1: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.primary-dark"),
             },
             h2: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.primary-dark"),
             },
             h3: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.primary-dark"),
             },
             h4: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.primary-dark"),
             },
             h5: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.primary-dark"),
             },
             h6: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.primary-dark"),
             },
-
             strong: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.primary-dark"),
             },
-
             code: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.primary-dark"),
             },
-
             figcaption: {
-              color: theme("colors.gray.500"),
+              color: theme("colors.secondary-dark"),
             },
-
             blockquote: {
-              color: theme("colors.gray.300"),
+              color: theme("colors.primary-dark"),
             },
           },
         },
